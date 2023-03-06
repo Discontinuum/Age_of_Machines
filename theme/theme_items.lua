2cf61625e727ad94cc79417d364e8f8f62f5eff9
User <>
2023-03-06 15:18:10 +0400

    Metal and coal in the theme

--- theme/theme_items.lua
+++ theme/theme_items.lua
@@ -4,22 +4,34 @@ local colors = { ---blue = args.ge_blue ,
 		 red = '#fa8c50',
 		 gray = '#808080' }
 
----------------
--- Metal
----------------
-
 
 function wesnoth.interface.game_display.metal()
-	-- Display research points for the viewing side, not the current side
+	-- Display for the viewing side, not the current side
 	local viewing_side = wesnoth.interface.get_viewing_side()
 
-	-- Find the existing research points of the side
 	local side_proxy = wesnoth.sides[viewing_side]
 	local mtl = side_proxy["variables"]["metal_reserve"] or 0
-	local str = mtl --research_points .. ' RP (+' .. rp_production .. ')'
-	if (viewing_side ~= wesnoth.current.side) then
-		str = "<span color='" .. colors.gray .. "'>" .. str .. "</span>"
-	end
+	local str = mtl
+	--if (viewing_side ~= wesnoth.current.side) then
+	--	str = "<span color='" .. colors.gray .. "'>" .. str .. "</span>"
+	--end
+
+	return { { 'element', {
+		text = str,
+		tooltip = "The side's metal reserve"
+	} } }
+
+end
+
+function wesnoth.interface.game_display.coal()
+	-- Display for the viewing side, not the current side
+	local viewing_side = wesnoth.interface.get_viewing_side()
+
+	local mtl = wml["variables"]["coal_reserve" .. viewing_side] or 0
+	local str = mtl
+	--if (viewing_side ~= wesnoth.current.side) then
+	--	str = "<span color='" .. colors.gray .. "'>" .. str .. "</span>"
+	--end
 
 	return { { 'element', {
 		text = str,
